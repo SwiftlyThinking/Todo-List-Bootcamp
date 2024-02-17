9ad459cb264c39f3cd6fe10ec51f0a7c27dad105
Mykyta Kuzminov <mykyta.kuzminov26@icloud.com>
2024-02-17 11:34:22 +0200

    Set up a project

---
+++ TodoListBootcamp/TodoListBootcamp/TodoListBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListBootcampApp.swift
+//  TodoListBootcamp
+//
+//  Created by Mykyta Kuzminov on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
